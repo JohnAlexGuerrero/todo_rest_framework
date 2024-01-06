cb19f47646abe99e14a17bb4a902d617dde8cf1c
johnalexguerrero <johnalexguerrero@gmail.com>
2024-01-05 21:18:12 -0500

    database postgrest

--- todo/settings.py
+++ todo/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku: Actualice la configuración de la base de datos desde $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
